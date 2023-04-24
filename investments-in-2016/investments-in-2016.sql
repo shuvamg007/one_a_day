eb48a6caa5108459a527a97491a5cab4125a9395
Suvobrat Ghosh <shuvamg007@gmail.com>
2023-04-24 16:39:22 -0400

    Time: 995 ms (48.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+select round(sum(tiv_2016), 2) as tiv_2016
+from Insurance
+where tiv_2015 in
+( select tiv_2015 from Insurance group by tiv_2015 having count(tiv_2015) > 1 )
+and 
+concat(lat, lon) in
+( select concat(lat, lon) from Insurance group by lat, lon having count(concat(lat, lon)) = 1 )
+
+  # , varchar(lat) + ' ' + 
+  # varchar(long) from Insurance;
+# SELECT pid, count((lat + long)) from 

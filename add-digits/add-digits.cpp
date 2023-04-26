176689c253f8c5c2f79d363feb366e1ed5728775
Suvobrat Ghosh <shuvamg007@gmail.com>
2023-04-25 20:53:43 -0400

    Time: 3 ms (56.14%) | Memory: 6 MB (63.10%) - LeetSync

--- add-digits/add-digits.cpp
+++ add-digits/add-digits.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while (num / 10 >= 1) {
-            int tmp = 0;
-            while (num > 0) {
-                tmp = tmp + (num % 10);
-                num = int(num / 10);
-            }
-            num = tmp;
-        }
-
-        return num;
+        return (num - 1) % 9 + 1;
     }
 };

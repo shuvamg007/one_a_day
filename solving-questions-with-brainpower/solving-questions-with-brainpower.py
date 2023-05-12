4c743e73db5858eea9e411df1f2ff43d26f57456
Suvobrat Ghosh <shuvamg007@gmail.com>
2023-05-11 20:49:37 -0400

    Time: 1761 ms (28.81%) | Memory: 147.7 MB (41.98%) - LeetSync

--- solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,17 +1,17 @@
 class Solution:
-    def mostPoints(self, questions: List[List[int]]) -> int:     
+    def mostPoints(self, questions: List[List[int]]) -> int:
         n = len(questions)
         dp = [0] * n
-        
-        def dfs(i):
-            if i >= n:
-                return 0
-            if dp[i]:
-                return dp[i]
-            points, skip = questions[i]
 
-            # dp[i] = max(skip it, solve it)
-            dp[i] = max(dfs(i + 1), points + dfs(i + skip + 1))
-            return dp[i]
-        
-        return dfs(0)
+        def recursion(idx: int) -> int:
+            if idx >= n:
+                return 0
+            if dp[idx]:
+                return dp[idx]
+
+            val, skip = questions[idx]
+            dp[idx] = max(recursion(idx + 1), val + recursion(idx + skip + 1))
+
+            return dp[idx]
+
+        return recursion(0)

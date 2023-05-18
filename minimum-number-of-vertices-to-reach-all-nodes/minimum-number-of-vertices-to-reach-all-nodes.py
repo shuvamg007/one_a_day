596a0954bcedc3a56e0f9dc8bfc71c4dcb5d6b93
Suvobrat Ghosh <shuvamg007@gmail.com>
2023-05-17 20:32:20 -0400

    Time: 1205 ms (37.64%) | Memory: 55 MB (20.27%) - LeetSync

--- minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,26 +1,7 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        op, visited = set(), set()
-        def dfs(n):
-            if n in visited:
-                if n in op:
-                    op.remove(n)
-                return
-            op.add(n)
-            visited.add(n)
+        exclude = set()
+        for a, b in edges:
+            exclude.add(b)
 
-            for j in nodes[n]:
-                if j in op:
-                    op.remove(j)
-
-                dfs(j)
-
-
-        nodes = defaultdict(list)
-        for x, y in edges:
-            nodes[x].append(y)
-
-        for i in range(n):
-            dfs(i)
-
-        return list(op)
+        return [x for x in range(n) if x not in exclude]

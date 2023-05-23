e678078a77f7f477e1fcecb307879f1d3f8138a4
Suvobrat Ghosh <shuvamg007@gmail.com>
2023-05-23 10:04:12 -0400

    Time: 2237 ms (50.19%) | Memory: 0B (100.00%) - LeetSync

--- movie-rating/movie-rating.sql
+++ movie-rating/movie-rating.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-with temp1 as (
+(
     select Users.name as results
     from Users
     inner join MovieRating
@@ -7,18 +7,17 @@ with temp1 as (
     group by Users.user_id
     order by count(*) desc, results asc
     limit 1
-), temp2 as (
+)
+UNION ALL
+(
     select Movies.title as results
     from Movies
     inner join MovieRating
     on Movies.movie_id = MovieRating.movie_id
-    where YEAR(MovieRating.created_at) = "2020" and MONTH(MovieRating.created_at) = '2'
+    where MovieRating.created_at like "2020-02%"
     group by Movies.movie_id
     order by avg(MovieRating.rating) desc, results asc
     limit 1
 )
-select * from temp1
-union all
-select * from temp2
 
 

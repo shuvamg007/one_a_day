ed6d853f6d0679850a14d504aa4c74629813ae2c
Suvobrat Ghosh <shuvamg007@gmail.com>
2023-05-28 18:53:06 -0400

    Time: 2457 ms (11.58%) | Memory: 20.3 MB (15.55%) - LeetSync

---
+++ minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts = sorted(cuts)
+        memo = dict()
+
+        def recursive(start, end, cuts):
+            nonlocal memo
+            if not len(cuts):
+                return 0
+
+            if (start, end) in memo:
+                return memo[(start, end)]
+
+            min_cost = float('inf')
+            for idx in range(len(cuts)):
+                cut = cuts[idx]
+                # new_cuts = cuts[:idx] + cuts[idx+1:]
+                min_cost = min(min_cost, end - start + recursive(start, cut, cuts[:idx]) + recursive(cut, end, cuts[idx+1:]))
+                # print(start, end, recursive(0, cut, cuts[:idx]), recursive(cut, n, cuts[idx+1:]))
+
+            memo[(start, end)] = min_cost
+            return min_cost
+
+        min_cost = float('inf')
+        for idx in range(len(cuts)):
+            cut = cuts[idx]
+            # new_cuts =  + 
+            min_cost = min(min_cost, n + recursive(0, cut, cuts[:idx]) + recursive(cut, n, cuts[idx+1:]))
+            print(n, recursive(0, cut, cuts[:idx]), recursive(cut, n, cuts[idx+1:]))
+        return min_cost

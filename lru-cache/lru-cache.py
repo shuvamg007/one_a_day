3765dd6f56a41027e149c459b24c49e442219f81
Suvobrat Ghosh <shuvamg007@gmail.com>
2023-06-15 18:32:31 -0400

    Time: 795 ms (69.68%) | Memory: 83.9 MB (5.61%) - LeetSync

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -1,40 +1,32 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.map = dict()
-        self.q = collections.deque()
         self.counter = 0
-        self.capacity = capacity
-        
+        self.cap = capacity
+        self.hmap = dict()
+        self.q = collections.deque() 
 
     def get(self, key: int) -> int:
-        if key in self.map:
-            val, countr = self.map[key]
-
-            self.map[key] = (val, self.counter)
+        if key in self.hmap:
+            val = self.hmap[key][0]
+            self.hmap[key] = (val, self.counter)
             self.q.append((key, self.counter))
 
             self.counter += 1
-            return self.map[key][0]
 
+            return val
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.map:
-            self.map[key] = (value, self.counter)
-            self.q.append((key, self.counter))
-            self.counter += 1
+        if key not in self.hmap:
+            while len(self.hmap) >= self.cap:
+                k, _counter = self.q.popleft()
+                if _counter == self.hmap[k][1]:
+                    del self.hmap[k]
 
-        else:
-            while len(self.map) >= self.capacity:
-                k, countr = self.q.popleft()
-                if self.map[k][1] == countr:
-                    del self.map[k]
-            self.map[key] = (value, self.counter)
-            self.q.append((key, self.counter))
-            self.counter += 1
-
+        self.hmap[key] = (value, self.counter)
+        self.q.append((key, self.counter))
+        self.counter += 1
 
 
 # Your LRUCache object will be instantiated and called as such:
